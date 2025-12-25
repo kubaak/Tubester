69e9bb0c67d54a791efb68af5fbb6f159edf1418
Jakub Heidtke <hkubaak@gmail.com>
2025-12-25 23:18:52 +0100

    feat: comments and readme

--- YouTubester.Abstractions/Channels/UserChannelDto.cs
+++ YouTubester.Abstractions/Channels/UserChannelDto.cs
@@ -1,5 +1,11 @@
 namespace YouTubester.Abstractions.Channels;
 
+/// <summary>
+/// Represents a channel that belongs to a specific user account.
+/// </summary>
+/// <param name="Id">Identifier of the channel.</param>
+/// <param name="Title">Display title of the channel.</param>
+/// <param name="Picture">Optional uniform resource locator of the channel picture.</param>
 public sealed record UserChannelDto(
     string Id,
     string Title,
